958d8f266cc562e9912fc218927618c1001cd547
20essentials <s0l0estudi4r@gmail.com>
2024-06-07 19:51:17 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("Based in: ", "https://youtu.be/TktPJwQ1o0M?si=u2TjnNwixrxm1Jqf");
+console.log("Campo: ", "https://img.freepik.com/vector-gratis/campo-futbol-verde_225004-1137.jpg");
+console.log("Ball: ", "https://pics.clipartpng.com/Soccer_Ball_PNG_Clip_Art-1361.png");
